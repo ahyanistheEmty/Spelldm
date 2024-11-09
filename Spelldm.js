2bef1962424e6391a111ebdec3bf114da8e03cfe
ahyanistheEmty <156690076+ahyanistheEmty@users.noreply.github.com>
2024-11-09 11:23:27 +0530

    Update Spelldm.js

--- Spelldm.js
+++ Spelldm.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
         summarizeButton.classList.add('editing');
 
         try {
-            const response = await fetch('https://dbwlqflmg-3002.inc1.devtunnels.ms/summarize', {
+            const response = await fetch('https://9blxjqfd-3002.inc1.devtunnels.ms/summarize', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
